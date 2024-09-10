f5ac223e41a267f706ca0faeb95984df2a4257ec
H Sareen Kumar <sareenh10@gmail.com>
2024-09-10 22:51:57 +0530

    Create ecs.tf

---
+++ ecs.tf
@@ -0,0 +1,33 @@
+resource "aws_ecs_cluster" "medusa_cluster" {
+  name = "medusa-cluster"
+}
+
+resource "aws_ecs_task_definition" "medusa_task" {
+  family                   = "medusa-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name  = "medusa-backend"
+    image = "medusajs/medusa"
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
+}
+
+resource "aws_ecs_service" "medusa_service" {
+  name            = "medusa-service"
+  cluster         = aws_ecs_cluster.medusa_cluster.id
+  task_definition = aws_ecs_task_definition.medusa_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.medusa_subnet.id]
+    security_groups = [aws_security_group.medusa_sg.id]
+  }
+}
